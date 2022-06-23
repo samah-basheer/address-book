aa6e08bd0cba99c19e9433e2d0b60e5c15318ca2
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-23 12:42:59 +0300

    connect to mongo db

--- server/app.js
+++ server/app.js
@@ -1,19 +1,26 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require('express');
+const morgan = require("morgan");
+const connectDB = require("./config/db");
 
-const app = express()
+const app = express();
 
 // middlewares
+app.use(express.json());
+app.use(morgan("tiny"));
 
 // routes
 app.get("/", (req, res) => {
-    res.send("hello world")
+    res.send("hello world");
 })
 
 // server configurations 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    // connectDB();
-    console.log(`server listening on port: ${PORT}`);
+app.listen(PORT, async() => {
+    try {
+        await connectDB();
+        console.log(`server listening on port: ${PORT}`);
+    } catch(err) {
+        console.log(err)
+    }
 });
