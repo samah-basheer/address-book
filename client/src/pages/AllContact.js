4987678184d656a0b60c8bd419e3676c711a4da5
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-25 21:53:52 +0300

    added filtering option

--- client/src/pages/AllContact.js
+++ client/src/pages/AllContact.js
@@ -3,9 +3,18 @@ import './Home.css';
 import './AllContact.css';
 
 const AllContact = () => {
-    const [contacts, setContacts] = useState([
+    const [contacts, setContacts] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
 
-    ]);
+    const handleSearchSubmit = event => {
+        event.preventDefault();
+
+        const newSearchUser = contacts.filter((contact) =>
+            contact.name.toLowerCase().includes(searchInput.toLowerCase())
+        );
+        setContacts(newSearchUser);
+    }
+
 
     useEffect(async () => {
         try {
@@ -36,30 +45,43 @@ const AllContact = () => {
                 contacts.length === 0 ? (
                     <h5>No contacts created yet.</h5>
                 ) : (
-                    <div className='table-wrapper'>
-                        <table className="fl-table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Address</th>
-                                    <th>Email</th>
-                                    <th>Phone Number</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    contacts.map(contact => (
-                                        <tr key={contact._id}>
-                                            <td>{contact.name}</td>
-                                            <td>{contact.address}</td>
-                                            <td>{contact.email}</td>
-                                            <td>{contact.phone}</td>
-                                        </tr>
-                                    ))
-                                }
-                            </tbody>
-                        </table>
-                    </div>
+                    <>
+                        <form className='form-flex' onSubmit={handleSearchSubmit}>
+                            <div className='form-group'>
+                                <input type='text' name='searchInput' id='searchInput' className='form-control' value={searchInput} onChange={
+                                    (e) => setSearchInput(e.target.value)
+                                } placeholder='Search Contact' />
+                            </div>
+                            <div className='submit-btn'>
+                                <input type='submit' className='btn' value='Search' />
+                            </div>
+                        </form>
+                        <p className='total'>Your total: <strong>{contacts.length}</strong></p>
+                        <div className='table-wrapper'>
+                            <table className="fl-table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Address</th>
+                                        <th>Email</th>
+                                        <th>Phone Number</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        contacts.map(contact => (
+                                            <tr key={contact._id}>
+                                                <td>{contact.name}</td>
+                                                <td>{contact.address}</td>
+                                                <td>{contact.email}</td>
+                                                <td>{contact.phone}</td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </>
                 )
             }
 
