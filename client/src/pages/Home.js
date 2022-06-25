a620481196c668cecea8afd397f1a27f75f7bfd3
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-25 09:23:36 +0300

    added navigate login in home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,13 @@
+import { useEffect, useContext, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
+
 const Home = () => {
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+    useEffect(() => {
+        !user && navigate("/login", { replace: true });
+    }, [])
     return (<>
         This is home page
     </>);
